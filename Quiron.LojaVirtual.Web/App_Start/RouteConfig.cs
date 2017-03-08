620650cc7eedffdce939820eaf877a6079a43f3f
devmedialoja2017 <robronny@gmail.com>
2017-03-08 16:32:58 -0300

    View Vitrine

--- Quiron.LojaVirtual.Web/App_Start/RouteConfig.cs
+++ Quiron.LojaVirtual.Web/App_Start/RouteConfig.cs
@@ -16,7 +16,8 @@ namespace Quiron.LojaVirtual.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                //defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Produto", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
